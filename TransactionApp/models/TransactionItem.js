e3fb2a52165c11b92e22466de840933ee88c2476
Efren Haskell <efrenhask@gmail.com>
2023-04-17 02:31:47 -0400

    created transaction schema

--- TransactionApp/models/TransactionItem.js
+++ TransactionApp/models/TransactionItem.js
@@ -1,1 +1,13 @@
-ECHO is on.
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var transactionItemSchema = Schema( {
+  description: String,
+  ammount: Number,
+  category: String,
+  date: Date,
+} );
+
+module.exports = mongoose.model( 'transactionItemSchema', transactionItemSchema );
