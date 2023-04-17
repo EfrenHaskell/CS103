105b1e042f5874e695213d4bb8d05b02060677e2
Efren Haskell <efrenhask@gmail.com>
2023-04-17 02:32:38 -0400

    added router get request for transaction page

--- TransactionApp/routes/transaction.js
+++ TransactionApp/routes/transaction.js
@@ -1,1 +1,19 @@
-ECHO is on.
+const express = require('express');
+const router = express.Router();
+const User = require('../models/TransactionItem')
+
+isLoggedIn = (req,res,next) => {
+    if (res.locals.loggedIn) {
+      next()
+    } else {
+      res.redirect('/login')
+    }
+  }
+
+router.get('/transaction',
+    isLoggedIn,
+    async (req, res, next) => {
+        res.render('transaction');
+ });
+    
+module.exports = router;
