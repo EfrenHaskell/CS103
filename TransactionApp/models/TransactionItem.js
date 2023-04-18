ed9a7371dcbf12e7193e404c771c6c57233e04d6
Efren Haskell <efrenhask@gmail.com>
2023-04-17 23:02:17 -0400

    added edit and remove capabilities for transactions

--- TransactionApp/models/TransactionItem.js
+++ TransactionApp/models/TransactionItem.js
@@ -1,13 +1,13 @@
 'use strict';
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.Schema.Types.ObjectId;
+//const ObjectId = mongoose.Schema.Types.ObjectId;
 
 var transactionItemSchema = Schema( {
   description: String,
-  ammount: Number,
+  amount: String,
   category: String,
-  date: Date,
+  date: String,
 } );
 
 module.exports = mongoose.model( 'transactionItemSchema', transactionItemSchema );
--- TransactionApp/routes/transaction.js
+++ TransactionApp/routes/transaction.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/TransactionItem')
+const User = require('../models/TransactionItem');
+const TransactionItem = require('../models/TransactionItem');
 
 isLoggedIn = (req,res,next) => {
     if (res.locals.loggedIn) {
@@ -10,10 +11,55 @@ isLoggedIn = (req,res,next) => {
     }
   }
 
-router.get('/transaction',
+router.get('/transaction/',
     isLoggedIn,
     async (req, res, next) => {
-        res.render('transaction');
+      let items = await TransactionItem.find({}).sort({date:1});
+      res.render('Transaction',{items});
  });
-    
+
+router.post('/transaction',
+isLoggedIn,
+async (req, res, next) => {
+  //const currData = new Date();
+  const transaction = new TransactionItem (
+    {
+      description: req.body.description,
+      amount: req.body.amount,
+      category: req.body.category,
+      date: new Date(req.body.date).toDateString()
+    })
+  await transaction.save();
+  res.redirect('/transaction')
+});
+
+router.get('/transaction/remove/:itemId',
+    isLoggedIn,
+    async (req, res, next) => {
+      console.log("inside /todo/remove/:itemId")
+      await TransactionItem.deleteOne({_id:req.params.itemId});
+      res.redirect('/transaction')
+});
+
+
+router.get('/transaction/editTransaction/:itemId',
+  isLoggedIn,
+  async (req, res, next) => {
+      console.log("inside /transaction/editTransaction/:itemId")
+      const item = await TransactionItem.findById(req.params.itemId);
+      res.locals.item = item;
+      res.render('editTransaction');
+});
+
+router.post('/transaction/updateTransaction',
+  isLoggedIn,
+  async (req, res, next) => {
+    const {itemId,description,amount,category} = req.body;
+    console.log("inside /transaction/updateTransaction/:itemId");
+    await TransactionItem.findOneAndUpdate(
+      {_id:itemId},
+      {$set: {description,amount,category}} );
+  res.redirect('/transaction')
+});
+
 module.exports = router;
